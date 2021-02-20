aa1995870051cfc95c9b58cefeae5cdab06d1e4f
Jiexiu Zhu <jz4919@ic.ac.uk>
2021-02-20 23:08:35 +0800

    resolve flake8 errors

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,39 +1,45 @@
+"""Define new class Polynomial."""
+
+
 from numbers import Number
 
 
 class Polynomial:
-    def __init__(self, coefs):
-        
-        self.coefficients=coefs
-    
-    def degree(self):
+    """Define new class."""
 
+    def __init__(self, coefs):
+        """Input coefficients."""
+        self.coefficients = coefs
+
+    def degree(self):
+        """Define degree."""
         return len(self.coefficients) - 1
 
     def __str__(self):
-
+        """Make class readable."""
         coefs = self.coefficients
-        terms =[]
+        terms = []
 
         if coefs[0]:
             terms.append(str(coefs[0]))
         if self.degree() and coefs[1]:
             terms.append(f"{'' if coefs[1] ==1 else coefs[1]}x")
 
-        terms += [f"{''if c==1 else c}x^{d}" 
-                  for d, c in enumerate(coefs[2:],start=2) if c]
-        
+        terms += [f"{''if c==1 else c}x^{d}"
+                  for d, c in enumerate(coefs[2:], start=2) if c]
+
         return " + ".join(reversed(terms)) or "0"
 
     def __eq__(self, other):
-
+        """Define equality."""
         return self.coefficients == other.coefficients
-    
-    def __add__(self,other):
 
+    def __add__(self, other):
+        """Define addition."""
         if isinstance(other, Polynomial):
             common = min(self.degree(), other.degree()) + 1
-            coefs = tuple(a + b for a, b in zip(self.coefficients, other.coefficients))
+            coefs = tuple(a + b for a, b in zip(self.coefficients,
+                                                other.coefficients))
             coefs += self.coefficients[common:] + other.coefficients[common:]
 
             return Polynomial(coefs)
@@ -45,10 +51,6 @@ class Polynomial:
         else:
             return NotImplemented
 
-    def __radd__(self,other):
+    def __radd__(self, other):
+        """Define reverse addition."""
         return self + other
-    
-
-
-
-
